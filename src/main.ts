3b98c06cc99540a90c432e8f27d469ea900ee850
Vab Kapoor <vaib.kapoor15@gmail.com>
2024-10-08 00:06:30 -0400

    Renamed main.ts to sample.ts, emptied main class to just have basic code, moved to src/ folder

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Plugin, Notice } from 'obsidian'; // Import the Plugin class from Obsidian API
+
+export default class MultipleDailyNotes extends Plugin {
+  // Called when the plugin is loaded
+  async onload() {
+    console.log('Loading MyPlugin');
+    // this.addSettingTab(new DailyNotesSettingTab(this.app, this));
+    // Example: Add a simple command
+    this.addCommand({
+      id: 'example-command',
+      name: 'Example Command',
+      callback: () => {
+        new Notice('Hello from MyPlugin!');  // Show a message when the command is invoked
+      }
+    });
+  }
+  // Called when the plugin is unloaded (e.g., when disabled or removed)
+  onunload() {
+    console.log('Unloading MyPlugin');
+  }
+}
